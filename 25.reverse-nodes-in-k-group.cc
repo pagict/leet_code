796a72f8babff15afe7ab8d112a4ce7e0dd49709
pagict <pagict@gmail.com>
2022-08-27 23:37:57 +0800

    ac 25

--- 25.reverse-nodes-in-k-group.cc
+++ 25.reverse-nodes-in-k-group.cc
@@ -1,3 +1,6 @@
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
 #include <stack>
 #include <vector>
 using ListNode = struct ListNode {
@@ -9,23 +12,61 @@ using ListNode = struct ListNode {
 };
 
 ListNode *reverseKGroup(ListNode *head, int k) {
-  auto *fast_p = head;
-  for (auto i = 0u; i < k; ++i) {
-    if (fast_p == nullptr) {
-      return head;
-    }
-    fast_p = fast_p->next;
-  }
-
   ListNode fake_head(0, head);
-  auto *p = head;
+  auto *p = &fake_head;
   std::stack<ListNode *, std::vector<ListNode *>> node_stack;
   while (p) {
-    auto *tmp = p;
+    ListNode *fast_p = p->next;
+    for (auto i = 0u; i < k; ++i) {
+      if (fast_p == nullptr) {
+        return fake_head.next;
+      }
+      fast_p = fast_p->next;
+    }
+
+    auto *tmp = p->next;
     while (tmp != fast_p) {
       node_stack.push(tmp);
       tmp = tmp->next;
     }
+
+    while (!node_stack.empty()) {
+      auto *tmp = node_stack.top();
+      node_stack.pop();
+      p->next = tmp;
+      p = tmp;
+    }
+    p->next = fast_p;
   }
-  return nullptr;
+  return fake_head.next;
+}
+
+void printList(ListNode *head) {
+  if (!head) {
+    return;
+  }
+  auto *p = head;
+  while (p) {
+    printf("%d", p->val);
+    if (p->next) {
+      printf("->");
+    }
+    p = p->next;
+  }
+  printf("\n");
+}
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    return -1;
+  }
+  int k = std::atoi(argv[1]);
+  ListNode fake_head(0);
+  auto *p = &fake_head;
+  for (auto i = 2u; i < argc; ++i) {
+    p->next = new ListNode(std::atoi(argv[i]));
+    p = p->next;
+  }
+
+  printList(reverseKGroup(fake_head.next, k));
 }
