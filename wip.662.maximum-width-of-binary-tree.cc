47813ceb5b634c411e3aced44791a57e0e758213
pagict <pagict@gmail.com>
2022-08-28 23:11:16 +0800

    wip 662

--- wip.662.maximum-width-of-binary-tree.cc
+++ wip.662.maximum-width-of-binary-tree.cc
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <string>
 #include <vector>
 
 using TreeNode = struct TreeNode {
@@ -12,6 +13,7 @@ using TreeNode = struct TreeNode {
 };
 
 int widthOfBinaryTree(TreeNode *root) {
+  printf("=====\n");
   if (!root) {
     return 0;
   }
@@ -20,9 +22,37 @@ int widthOfBinaryTree(TreeNode *root) {
   auto height = 0u;
   auto max_in_level = std::pow(2, height);
   auto level_cnt = 0;
+
+  auto max_width = 0;
+
   while (!queue.empty()) {
     auto *p = queue.front();
     queue.erase(queue.begin());
+    while (level_cnt >= max_in_level) {
+      level_cnt -= max_in_level;
+      ++height;
+      max_in_level = std::pow(2, height);
+      if (valid_nodes > 0 && !queue.empty()) {
+        auto i = 0u;
+        for (; i < queue.size(); ++i) {
+          if (queue[i])
+            break;
+        }
+        auto j = queue.size() - 1;
+        for (; j >= 0; --j) {
+          if (queue[j])
+            break;
+        }
+        auto width = j; // or j
+        if (width > max_width) {
+          max_width = width;
+        }
+      }
+    }
+    printf("num(%s) at height:%d, cnt:%u\n",
+           p ? std::to_string(p->val).c_str() : "null", height, level_cnt);
+    ++level_cnt;
+
     if (p) {
       queue.push_back(p->left);
       queue.push_back(p->right);
@@ -34,7 +64,46 @@ int widthOfBinaryTree(TreeNode *root) {
       }
       --valid_nodes;
     } else {
+      if (valid_nodes) {
+        queue.push_back(nullptr);
+        queue.push_back(nullptr);
+      }
     }
   }
-  return 0;
+  return max_width;
+}
+
+int main(int argc, char **argv) {
+  auto *root = new TreeNode(
+      3,
+      new TreeNode(
+          4, new TreeNode(2),
+          new TreeNode(9, nullptr, new TreeNode(7, nullptr, new TreeNode(8)))),
+      new TreeNode(
+          4, new TreeNode(2),
+          new TreeNode(5, nullptr, new TreeNode(7, nullptr, new TreeNode(8)))));
+  printf("max_width=%d\n", widthOfBinaryTree(root));
+
+  root = new TreeNode(1, new TreeNode(2, nullptr, new TreeNode(4)),
+                      new TreeNode(3));
+  printf("max_width=%d\n", widthOfBinaryTree(root));
+
+  root = new TreeNode(1, new TreeNode(2), nullptr);
+  printf("max_width=%d\n", widthOfBinaryTree(root));
+
+  root = new TreeNode(
+      1, new TreeNode(2, new TreeNode(3, new TreeNode(4), nullptr), nullptr),
+      new TreeNode(5));
+  printf("max_width=%d\n", widthOfBinaryTree(root));
+
+  root = new TreeNode(1);
+  printf("max_width=%d\n", widthOfBinaryTree(root));
+
+  root = new TreeNode(
+      3, nullptr,
+      new TreeNode(30,
+                   new TreeNode(10, nullptr,
+                                new TreeNode(15, nullptr, new TreeNode(45))),
+                   nullptr));
+  printf("max_width=%d\n", widthOfBinaryTree(root));
 }
